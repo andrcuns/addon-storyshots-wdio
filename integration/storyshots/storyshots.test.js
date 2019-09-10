7e58577fd2eb71e3c6c69d319bd43b48d95ff2b8
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2019-09-10 13:12:17 +0300

    Dockerize browser for integration test

--- integration/storyshots/storyshots.test.js
+++ integration/storyshots/storyshots.test.js
@@ -1,34 +1,21 @@
-/* eslint-disable no-undef */
 import path from 'path';
 
 import initStoryshots from '@storybook/addon-storyshots';
-import { start } from 'chromedriver';
 import { imageSnapshot } from '../../src/index';
-
-let driver;
-
-beforeAll(async () => {
-  driver = await start([], true);
-});
-
-afterAll(async () => {
-  await driver.kill();
-});
 
 initStoryshots({
   suite: 'Default story snapshots',
   configPath: path.join(__dirname, '../storybook'),
   framework: 'react',
   test: imageSnapshot({
-    storybookUrl: `file:///${path.join(__dirname, '../../.storybook')}`,
+    storybookUrl: 'file:///app/.storybook',
     getMatchOptions: () => ({
       failureThreshold: 0.002,
       failureThresholdType: 'percent',
     }),
     browserOptions: {
       hostname: 'localhost',
-      port: 9515,
-      path: '/',
+      port: 4444,
       logLevel: 'error',
       capabilities: {
         browserName: 'chrome',
