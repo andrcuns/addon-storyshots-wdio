122d3a41b700ab39f84487ce1d803ec1f94aa3d1
Andrejs <andrejs.cunskis@gmail.com>
2019-11-08 01:04:37 +0200

    Update versions (#81)
    * Update versions
    
    * Fix release drafter

--- integration-test.sh
+++ integration-test.sh
@@ -5,7 +5,7 @@ yarn build-storybook
 docker run -d --name=grid -p 4444:24444 \
   -v /dev/shm:/dev/shm  \
   -v $PWD:/app \
-  elgalu/selenium:3.141.59-p21
+  elgalu/selenium:3.141.59-p25
 
 docker exec grid wait_all_done 30s
 
